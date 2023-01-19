8f0ead073280015a621a33b5ee75a7d631ff3ed1
Maxime Balan <maxime.balan@gmail.com>
2023-01-19 10:01:00 +0100

    survey repo

--- src/main/java/canard/intern/post/following/backend/repository/SurveyRepository.java
+++ src/main/java/canard/intern/post/following/backend/repository/SurveyRepository.java
@@ -1,7 +1,6 @@
-package canard.intern.post.following.backend.repository;
+	package canard.intern.post.following.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import canard.intern.post.following.backend.entity.Survey;
 
 public interface SurveyRepository extends JpaRepository<Survey, Integer>{
