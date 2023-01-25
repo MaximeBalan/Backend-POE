5f256d7254146b28f022683623b67398a808d99e
AnthonyA <alaguin25@gmail.com>
2023-01-25 10:37:36 +0100

    authentification utilisateur

--- src/main/java/canard/intern/post/following/backend/config/BasicAuthenticationConfig.java
+++ src/main/java/canard/intern/post/following/backend/config/BasicAuthenticationConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,19 +16,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class BasicAuthenticationConfig {
 	
-	
-	@Autowired
-	  private AppBasicAuthenticationEntryPoint authenticationEntryPoint;
-
 	  @Bean
 	  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	    http.authorizeRequests()
+		  return  http
+			.csrf()
+			.disable()
+			.authorizeRequests()
 	        .antMatchers("/login","/public/**").permitAll()
 	        .anyRequest().authenticated()
 	        .and()
 	        .httpBasic()
-	        .authenticationEntryPoint(authenticationEntryPoint);
-	    return http.build();
+	        .and()
+	        .sessionManagement()
+	        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        .and()
+	        .build();
 	  }
 
 	  @Bean
